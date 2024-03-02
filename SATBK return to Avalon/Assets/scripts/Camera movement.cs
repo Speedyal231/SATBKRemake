f79fa26ee27a66bcefb4afb208cadba46084a103
Speedyal231 <144699723+Speedyal231@users.noreply.github.com>
2024-03-02 03:39:05 +0100

    camera mouse work

--- SATBK return to Avalon/Assets/scripts/Camera movement.cs
+++ SATBK return to Avalon/Assets/scripts/Camera movement.cs
@@ -13,6 +13,7 @@ public class Cameramovement : MonoBehaviour
     [SerializeField] float lookSpeed;
     [SerializeField] float lookDistance;
     private PlayerInputActions playerInputActions;
+    Vector3 currCamPos;
 
     private void Awake()
     {
@@ -21,12 +22,19 @@ public class Cameramovement : MonoBehaviour
         playerInputActions.Enable();
     }
 
+    private void Start()
+    {
+        Vector3 playerPos = player.transform.position + player.transform.up * 2;
+        camera.rotation = Quaternion.LookRotation(playerPos - camera.position, player.transform.up);
+        currCamPos = -player.transform.forward * camDistance;
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         //CamFollow();
         Vector2 look = GetLookVectorNormalized();
-        CamLook(look);
+        CamLook2(look);
     }
 
     private void CamFollow()
@@ -46,12 +54,30 @@ public class Cameramovement : MonoBehaviour
         camera.rotation = Quaternion.LookRotation(playerPos - camera.position, player.transform.up);
 
         Vector3 newpos = camera.position + (playerPos - camera.position) - (playerPos - camera.position).normalized * camDistance;
-
-        //camera.position = newpos;
+        camera.position = newpos;
         camera.RotateAround(playerPos, player.transform.up, look.x * lookDistance);
-        camera.RotateAround(playerPos, player.transform.right, look.y * lookDistance);
-        
+        camera.RotateAround(playerPos, camera.right, look.y * lookDistance);
+    }
 
+    private void CamLook2(Vector2 look)
+    {
+        Vector3 playerPos = player.transform.position + player.transform.up * 2;
+
+        //may need to flip x and y
+        float angleX = look.x * lookDistance;
+        float catenaX = 2 * camDistance * Mathf.Sin(angleX / 2);
+        Vector3 xChange = camera.forward.normalized * catenaX * Mathf.Cos(90 - (angleX / 2)) + camera.right.normalized * catenaX * Mathf.Sin(90 - (angleX / 2));
+
+        float angleY = look.y * lookDistance;
+        float catenaY = 2 * camDistance * Mathf.Sin(angleY / 2);
+        Vector3 YChange = camera.forward.normalized * catenaY * Mathf.Cos(90 - (angleY / 2)) + camera.up.normalized * catenaY * Mathf.Sin(90 - (angleY / 2));
+
+        Vector3 totalChange = YChange + xChange;
+
+        currCamPos += totalChange;
+
+        camera.position = player.transform.position + currCamPos;
+        camera.rotation = Quaternion.LookRotation(playerPos - camera.position, player.transform.up);
     }
 
     private Vector2 GetLookVectorNormalized()
